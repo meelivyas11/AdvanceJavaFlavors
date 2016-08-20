55fd04ad4b1f7c666758ba6cb4bdde3cd81b0340
collab_mvyas <Feb-2016>
2016-08-20 14:12:40 -0400

    removing Content

--- Meeli_Learning/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/AngularJS-Maven/js/script.js
+++
@@ -1,6 +0,0 @@
-
-// https://api.github.com/users/robconery 
-
-var myController = function($scope) {
-	$scope.message = "Angular Controller showing messages";
-};
--- Meeli_Learning/Maven-Plugin/src/main/java/com/collaborative/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.collaborative.service;
-
-import com.collaborative.domain.Contact;
-
-public interface AccountService {
-		
-		public Contact createAccount(String firstName, String lastName, String email, int phoneNumber);
-
-}
